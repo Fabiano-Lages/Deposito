293abf9c72504259f50edb06c48e9f9527114116
Fabiano Lages <fabiano.lages@tenti.com.br>
2024-04-26 03:34:23 -0300

    Inclusão do bcrypt para o login

--- src/public/scripts/scripts.js
+++ src/public/scripts/scripts.js
@@ -1,8 +1,2 @@
 window.addEventListener("load", () => {
-    document.querySelector("#frmProcurar").addEventListener("submit", (ev) => {
-        if(!document.querySelector("#txtProcurar").value) {
-            ev.preventDefault();
-            alert("Preencha algo antes de clicar no botão procurar");
-        }
-    });
 });
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,7 +1,35 @@
 const routerMain = require("express").Router();
 
 routerMain.get("/", (req, res) => {
-    res.render("home"); 
+    req.session.nome = "Fabiano";
+
+    res.render("home", {nome: req.session.nome}); 
 });
- 
+
+
+routerMain.get("/Vendas", (req, res) => {
+    res.render("Vendas", {nome: req.session.nome}); 
+});
+
+routerMain.get("/Produtos", (req, res) => {
+    res.render("Produtos"); 
+});
+
+routerMain.get("/Clientes", (req, res) => {
+    res.render("Clientes"); 
+});
+
+routerMain.post("/Procurar", (req, res) => {
+    res.render("Procurar"); 
+});
 module.exports = { routerMain };
+
+// const saltRounds = 10;
+// hashedPass = await bcrypt.hash(data.password, saltRounds);
+// data.password = hashedPass;
+
+
+// const isPasswordValid = await bcrypt.compare(data.password, user.password);
+// if (!isPasswordValid) {
+//     return res.status(401).json({ error: 'Invalid password' });
+// }
